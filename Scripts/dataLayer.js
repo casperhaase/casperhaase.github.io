d33d5ddb690a60611d37d603b2b5cf52d1263a00
Casper Haase <casperhaase1994@gmail.com>
2024-04-22 16:51:54 +0200

    view_item

--- Scripts/dataLayer.js
+++ Scripts/dataLayer.js
@@ -65,9 +65,9 @@ function viewItem() {
     window.dataLayer.push(
         {
             'event': 'view_item',
-            'value': itemViewed[0].price,
             'ecommerce': {
-                'items': itemViewed
+                'items': itemViewed,
+                'value': itemViewed[0].price
             }
         }
     )
